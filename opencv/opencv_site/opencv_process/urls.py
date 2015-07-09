9df598f1cf866a5400b0e8b11d6cf1601a38c69d
shawnaten <shawnmaten@gmail.com>
2015-07-09 15:54:10 -0500

    finished with django

--- opencv/opencv_site/opencv_process/urls.py
+++ opencv/opencv_site/opencv_process/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^process$', views.process, name='process'),
+    url(r'^result$', views.result, name='result'),
 ]
--- opencv/opencv_site/opencv_process/views.py
+++ opencv/opencv_site/opencv_process/views.py
@@ -1,4 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+
+from .forms import ImageForm
+
+from .opencv import featureMatch
 
 def process(request):
-    return HttpResponse("You're at the image process page.")
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            resultFile = featureMatch(request.FILES['item'], request.FILES['scene'])
+            with open(resultFile, "rb") as f:
+                return HttpResponse(f.read(), content_type="image/jpeg")
+    else:
+        form = ImageForm()
+
+    return render(request, 'opencv_process/process.html', {'form': form})
+
+def result(request):
+    return render(request, 'opencv_process/result.html')
