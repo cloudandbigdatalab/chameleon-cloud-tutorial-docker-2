1565366c8c729d73f295019c9ac7bf98f30e304f
shawnaten <shawnmaten@gmail.com>
2015-07-17 18:42:26 -0500

    working on compose

--- page/test_site/test_site/settings.py
+++ page/test_site/test_site/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'test_app',
 )
 
 MIDDLEWARE_CLASSES = (
