13f790626d7f6c3e745aefdb353784dd3ce65b74
shawnaten <shawnmaten@gmail.com>
2015-07-20 12:27:40 -0500

    working on displaying posts

--- page/test_site/test_app/views.py
+++ page/test_site/test_app/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from .forms import PostForm
+from .models import Post
 
 def index(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            print (form.cleaned_data['post_text'])
+            p = Post(post_text=form.cleaned_data['post_text'], pub_date=timezone.now())
+            p.save()
 
             return HttpResponseRedirect('/')
     else:
+        posts = Post.objects.all()
         form = PostForm()
 
-    return render(request, 'test_app/index.html', {'form': form})
+    return render(request, 'test_app/index.html', {'form': form, 'posts': posts})
