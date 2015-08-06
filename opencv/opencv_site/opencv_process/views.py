a55043a58e59b2edd7bd1e3c91042f6ae7ad5492
shawnaten <shawnmaten@gmail.com>
2015-08-06 15:13:09 -0500

    adding opencv files back

---
+++ opencv/opencv_site/opencv_process/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+
+from .forms import ImageForm
+
+from .opencv import featureMatch
+
+def process(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            resultFile = featureMatch(request.FILES['item'], request.FILES['scene'])
+            with open(resultFile, "rb") as f:
+                return HttpResponse(f.read(), content_type="image/jpeg")
+    else:
+        form = ImageForm()
+
+    return render(request, 'opencv_process/process.html', {'form': form})
--- processing/example.py
+++ processing/example.py
@@ -1,10 +1,11 @@
 import numpy
+import sys
 
-a = numpy.array[range(1,100)]
+a = numpy.arange(1,100)
 
 filename = sys.argv[0]
 
 text_file = open("/outdir/" + filename, "a")
 for i in a:
-    text_file.write(str(a) + "\n")
+    text_file.write(str(i) + "\n")
 text_file.close()
